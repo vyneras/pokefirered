ecf0b5ba5b3c10a2a9e552759d9c3858aa628535
Steven Yates <stevenyates0705@gmail.com>
2024-06-16 21:36:18 -0500

    Allow Running Without the Running Shoes
    Allowed the player to run from the start of the game before you have
    received the Running Shoes.

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -513,7 +513,7 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
         return;
     }
 
-    if ((heldKeys & B_BUTTON) && FlagGet(FLAG_SYS_B_DASH)
+    if ((heldKeys & B_BUTTON)
         && !IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior))
     {
         if (PlayerIsMovingOnRockStairs(direction))
