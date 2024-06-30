258569b7f5d10609a5c6637cb854ea85c4375c02
Steven Yates <stevenyates0705@gmail.com>
2024-06-30 14:33:01 -0500

    Fixed Issues with Trainer Battles
    Reverted more changes that were made to make single trainer battles be
    double battles.

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -48,9 +48,6 @@ enum
     TRAINER_PARAM_CLEAR_VAL_8BIT,
     TRAINER_PARAM_CLEAR_VAL_16BIT,
     TRAINER_PARAM_CLEAR_VAL_32BIT,
-    TRAINER_PARAM_WASTE_VAL_8BIT,
-    TRAINER_PARAM_WASTE_VAL_16BIT,
-    TRAINER_PARAM_WASTE_VAL_32BIT,
     TRAINER_PARAM_LOAD_SCRIPT_RET_ADDR,
 };
 
@@ -110,8 +107,8 @@ static const struct TrainerBattleParameter sOrdinaryBattleParams[] =
     {&sTrainerAIntroSpeech,         TRAINER_PARAM_LOAD_VAL_32BIT},
     {&sTrainerADefeatSpeech,        TRAINER_PARAM_LOAD_VAL_32BIT},
     {&sTrainerVictorySpeech,        TRAINER_PARAM_CLEAR_VAL_32BIT},
-    {&sTrainerCannotBattleSpeech,   TRAINER_PARAM_WASTE_VAL_32BIT},
-    {&sTrainerABattleScriptRetAddr, TRAINER_PARAM_WASTE_VAL_32BIT},
+    {&sTrainerCannotBattleSpeech,   TRAINER_PARAM_CLEAR_VAL_32BIT},
+    {&sTrainerABattleScriptRetAddr, TRAINER_PARAM_CLEAR_VAL_32BIT},
     {&sTrainerBattleEndScript,      TRAINER_PARAM_LOAD_SCRIPT_RET_ADDR},
 };
 
@@ -123,7 +120,7 @@ static const struct TrainerBattleParameter sContinueScriptBattleParams[] =
     {&sTrainerAIntroSpeech,         TRAINER_PARAM_LOAD_VAL_32BIT},
     {&sTrainerADefeatSpeech,        TRAINER_PARAM_LOAD_VAL_32BIT},
     {&sTrainerVictorySpeech,        TRAINER_PARAM_CLEAR_VAL_32BIT},
-    {&sTrainerCannotBattleSpeech,   TRAINER_PARAM_WASTE_VAL_32BIT},
+    {&sTrainerCannotBattleSpeech,   TRAINER_PARAM_CLEAR_VAL_32BIT},
     {&sTrainerABattleScriptRetAddr, TRAINER_PARAM_LOAD_VAL_32BIT},
     {&sTrainerBattleEndScript,      TRAINER_PARAM_LOAD_SCRIPT_RET_ADDR},
 };
@@ -137,7 +134,7 @@ static const struct TrainerBattleParameter sDoubleBattleParams[] =
     {&sTrainerADefeatSpeech,        TRAINER_PARAM_LOAD_VAL_32BIT},
     {&sTrainerVictorySpeech,        TRAINER_PARAM_CLEAR_VAL_32BIT},
     {&sTrainerCannotBattleSpeech,   TRAINER_PARAM_LOAD_VAL_32BIT},
-    {&sTrainerABattleScriptRetAddr, TRAINER_PARAM_WASTE_VAL_32BIT},
+    {&sTrainerABattleScriptRetAddr, TRAINER_PARAM_CLEAR_VAL_32BIT},
     {&sTrainerBattleEndScript,      TRAINER_PARAM_LOAD_SCRIPT_RET_ADDR},
 };
 
@@ -146,11 +143,11 @@ static const struct TrainerBattleParameter sOrdinaryNoIntroBattleParams[] =
     {&sTrainerBattleMode,           TRAINER_PARAM_LOAD_VAL_8BIT},
     {&gTrainerBattleOpponent_A,     TRAINER_PARAM_LOAD_VAL_16BIT},
     {&sTrainerObjectEventLocalId,   TRAINER_PARAM_LOAD_VAL_16BIT},
-    {&sTrainerAIntroSpeech,         TRAINER_PARAM_WASTE_VAL_32BIT},
+    {&sTrainerAIntroSpeech,         TRAINER_PARAM_CLEAR_VAL_32BIT},
     {&sTrainerADefeatSpeech,        TRAINER_PARAM_LOAD_VAL_32BIT},
     {&sTrainerVictorySpeech,        TRAINER_PARAM_CLEAR_VAL_32BIT},
-    {&sTrainerCannotBattleSpeech,   TRAINER_PARAM_WASTE_VAL_32BIT},
-    {&sTrainerABattleScriptRetAddr, TRAINER_PARAM_WASTE_VAL_32BIT},
+    {&sTrainerCannotBattleSpeech,   TRAINER_PARAM_CLEAR_VAL_32BIT},
+    {&sTrainerABattleScriptRetAddr, TRAINER_PARAM_CLEAR_VAL_32BIT},
     {&sTrainerBattleEndScript,      TRAINER_PARAM_LOAD_SCRIPT_RET_ADDR},
 };
 
@@ -768,18 +765,6 @@ static void TrainerBattleLoadArgs(const struct TrainerBattleParameter *specs, co
             break;
         case TRAINER_PARAM_CLEAR_VAL_32BIT:
             SetU32(specs->varPtr, 0);
-            break;
-        case TRAINER_PARAM_WASTE_VAL_8BIT:
-            SetU8(specs->varPtr, 0);
-            data += 1;
-            break;
-        case TRAINER_PARAM_WASTE_VAL_16BIT:
-            SetU16(specs->varPtr, 0);
-            data += 2;
-            break;
-        case TRAINER_PARAM_WASTE_VAL_32BIT:
-            SetU32(specs->varPtr, 0);
-            data += 4;
             break;
         case TRAINER_PARAM_LOAD_SCRIPT_RET_ADDR:
             SetPtr(specs->varPtr, data);
