5d8f57d38ebff06ebec5c71db9c0403e991f90b8
Steven Yates <stevenyates0705@gmail.com>
2025-03-13 18:34:16 -0500

    Allow 0 EXP
    Allow 0 EXP to be awarded to pokemon.

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -3197,18 +3197,11 @@ static void Cmd_getexp(void)
             if (viaExpShare) // at least one mon is getting exp via exp share
             {
                 *exp = SAFE_DIV(calculatedExp / 2, viaSentIn);
-                if (*exp == 0)
-                    *exp = 1;
-
                 gExpShareExp = calculatedExp / 2 / viaExpShare;
-                if (gExpShareExp == 0)
-                    gExpShareExp = 1;
             }
             else
             {
                 *exp = SAFE_DIV(calculatedExp, viaSentIn);
-                if (*exp == 0)
-                    *exp = 1;
                 gExpShareExp = 0;
             }
 
